030d0edfc9e40847e400920e0cf5520cb2d2bb1c
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2024-06-05 12:23:30 +0300

    comit

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -3,16 +3,16 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import styles from "./styles.module.css";
 
-const { container, wrapper } = styles;
+const { wrapper } = styles;
 const MainLayout = () => {
   return (
-    <Container className={container}>
+    <div className="app">
       <Header />
       <div className={wrapper}>
         <Outlet />
       </div>
       <Footer />
-    </Container>
+    </div>
   );
 };
 
