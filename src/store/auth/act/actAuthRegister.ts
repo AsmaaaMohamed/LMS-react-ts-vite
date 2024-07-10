fa0142109ed7a4f720d07f047722c9c71481ee08
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2024-07-10 22:14:39 +0300

    comit

--- src/store/auth/act/actAuthRegister.ts
+++ src/store/auth/act/actAuthRegister.ts
@@ -2,9 +2,15 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosErrorHandler from "@utils/axiosErrorHandler";
 import axios from "axios";
 
+type TFormData = {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
 const actAuthRegister = createAsyncThunk(
     "auth/actAuthRegister" ,
-    async( formData , thunk)=>{
+    async( formData:TFormData , thunk)=>{
         const {rejectWithValue} = thunk;
         try{
             const res = await axios.post("/register" , formData);
